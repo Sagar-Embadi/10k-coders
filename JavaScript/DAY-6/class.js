2b0ef192758f6eecefbc3a4d63c05fc989f4bacb
Sagar Embadi <sagarembadi7@gmail.com>
2024-12-10 12:13:15 +0530

    adding Day 6 files

---
+++ JavaScript/DAY-6/class.js
@@ -0,0 +1,41 @@
+// for(i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// for(i=1;i<=10;i++){
+//     if (i%2==0){
+//         console.log(i,"even");
+//     }else{
+//         console.log(i,"odd");
+//     }
+// }
+
+// var a=0;
+// for(i=1;i<=10;i++){
+//     a+=i
+// }
+// console.log(a);
+
+// var sumEven=0;
+// var sumOdd=0;
+// for(i=1;i<=10;i++){
+//     if (i%2==0){
+//         sumEven+=i
+//     }else{
+//         sumOdd+=i
+//     }
+// }
+// console.log(sumEven)
+// console.log(sumOdd)
+
+// var proEven=1;
+// var proOdd=1;
+// for(i=1;i<=10;i++){
+//     if (i%2==0){
+//         proEven*=i
+//     }else{
+//         proOdd*=i
+//     }
+// }
+// console.log(proEven)
+// console.log(proOdd)
