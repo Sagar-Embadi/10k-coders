862ddf35ab915289e73c3629a101db3688ba00dd
Sagar Embadi <sagarembadi7@gmail.com>
2024-12-18 16:24:11 +0530

    adding day 8 task files

---
+++ JavaScript/DAY-8/Day-8.js
@@ -0,0 +1,15 @@
+function movie(){
+    var a="hero_name"
+    var b="movie_name"
+    var c="director_name"
+    var d="plotof_movie"
+
+    var e={
+        hero_name:a,
+        movie_name:b,
+        director_name:c,
+        plotof_movie:d
+    }
+    console.log(e);
+}
+movie()
