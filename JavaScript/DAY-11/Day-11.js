4b8e0c8e10f407e094cb38b3c0aea8597ef6b966
Sagar Embadi <sagarembadi7@gmail.com>
2024-12-18 18:14:14 +0530

    Adding Day 8 Task files

---
+++ JavaScript/DAY-11/Day-11.js
@@ -0,0 +1,27 @@
+let tag1=document.getElementById("1");
+function click_bth(){
+    tag1.style.backgroundColor="red"
+    tag1.style.color="white"
+    tag1.style.border="5px solid black"
+    tag1.style.padding="15px"
+    tag1.style.textAlign="center"
+}
+
+let tag2=document.getElementById("2")
+function dbl_click(){
+    tag2.style.backgroundColor="green"
+    tag2.style.color="white"
+    tag2.style.border="5px solid black"
+    tag2.style.padding="15px"
+    tag2.style.textAlign="center"
+}
+
+let tag3=document.getElementById("3")
+function hover(){
+    tag3.style.backgroundColor="blue"
+    tag3.style.color="white"
+    tag3.style.border="5px solid black"
+    tag3.style.padding="15px"
+    tag3
+    .style.textAlign="center"
+}
