14a874eb8e4c1aab3d0f8326a7d40f42b3f59b62
Sagar Embadi <sagarembadi7@gmail.com>
2025-01-04 16:15:47 +0530

    uploading BOM class files

---
+++ JavaScript/BOM-2/login.js
@@ -0,0 +1,9 @@
+let form=document.getElementById("register")
+
+form.addEventListener("submit",(s)=>{
+    s.preventDefault()
+    let email=document.getElementById("email").value;
+    let pass=document.getElementById("password").value;
+    // console.log(name,email,pass);
+    
+})
