a261dcfdd6b1ee665352995849955f68897b5fb2
Sagar Embadi <sagarembadi7@gmail.com>
2025-01-08 09:38:37 +0530

    Uploading finding nearest prime problem

---
+++ Python/Class/nearestPrime.py
@@ -0,0 +1,31 @@
+'''
+    Finding prime numbers , if given number is not prime print left and right nearest prime for given number
+'''
+num = int(input("Enter a number: "))
+def isPrime(n):
+    if n>1:
+        for i in range(2,(n//2)+1):
+            if n%i ==0:
+                return False
+        return True
+    return False
+if (isPrime(num)):
+    print("It is Prime number")
+else:
+    lp,rp = num-1,num+1
+    while True:
+        if isPrime(lp):
+            lp=lp
+            break
+        lp -=1
+    while True:
+        if isPrime(rp):
+            rp=rp
+            break
+        rp+=1
+    if (rp-num) == (num-lp):
+        print(lp,rp)
+    elif (rp-num) < (num-lp):
+        print(rp)
+    else:
+        print(lp)
