d285f25d22a52dd9e8ad5db7a2b8df652fe16986
Sagar Embadi <sagarembadi7@gmail.com>
2025-01-08 22:53:27 +0530

    Uploading nearest Fibonacci Number problem

--- Python/Class/numConversion.py
+++ Python/Class/numConversion.py
@@ -32,3 +32,15 @@
 #     num = num//10
 #     count+=1
 # print(dec)
+
+'''
+    Converting Decimal into Binary
+'''
+num=int(input("Enter a number: "))
+bin1=""
+while num>0:
+    rem=num%2
+    bin1= str(rem)+bin1
+    num=num//2
+
+print(bin1)
---
+++ Python/TASK-3/fibonacciNum.py
@@ -0,0 +1,10 @@
+num=int(input("Enter a number: "))
+a,b=0,1
+i=1
+while a<=num:
+    c=a+b
+    a=b
+    b=c
+# print(a,b-a)
+res = "It is fib num" if b-a == num else b-a if (num-(b-a)<a-num) else a
+print(res)
