1dd39cc7feffc32f2f0abfc8870a42092f0f1108
Sagar Embadi <sagarembadi7@gmail.com>
2025-01-29 10:25:35 +0530

    UPdating Patterns

--- Python/Class/Patterns/leftTriangle.py
+++ Python/Class/Patterns/leftTriangle.py
@@ -20,6 +20,19 @@ for i in range(1,rows+1):
     for j in range(1,i+1):
         print(j,end=" ")
     print()
+
+'''
+    1 2 3 4
+    1 2 3 
+    1 2 
+    1
+'''
+rows = int(input("Enter rows: "))
+for i in range(rows,-1,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
 '''
     5 4 3 2 1 
     4 3 2 1 
