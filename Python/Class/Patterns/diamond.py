92c817f3572b44ad7eb6b37bc7359dbeadd0f70b
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-04 18:02:52 +0530

    adding diamond patterns

--- Python/Class/Patterns/diamond.py
+++ Python/Class/Patterns/diamond.py
@@ -48,7 +48,9 @@ for i in range(1,rows+1):
           * *
             *
 ''' 
+print()
 rows = int(input("Enter rows: "))
+print()
 for i in range(rows,-1,-1):
     print(" "*(2*(rows-i)),end="")
     print("* "*(i))
