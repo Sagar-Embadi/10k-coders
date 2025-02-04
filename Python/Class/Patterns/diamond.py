3619f742c8b3f6f926644a6a17ea267070b0c9ce
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-04 17:54:09 +0530

    adding diamond patterns

---
+++ Python/Class/Patterns/diamond.py
@@ -0,0 +1,54 @@
+'''
+         *
+        * *
+       * * *
+      * * * *
+       * * *
+        * *
+         *
+'''
+# rows = int(input("Enter rows: "))
+# for i in range(1,2*rows):
+#     stars = i if i<=rows else 2*rows-i
+#     spaces = rows-i if i<=rows else i-rows
+#     print(" "*spaces,end="")
+#     print("* "*stars,end="\n")
+#     # print()
+'''
+          1 
+        2 1 2
+      3 2 1 2 3
+    4 3 2 1 2 3 4
+'''
+# rows = int(input("Enter rows: "))
+# for i in range(1,rows+1):
+#     res = ""
+#     rev = ""
+#     print(" "*(2*(rows-i)),end="")
+#     for j in range(1,i+1):
+#         res+=str(j)+" "
+#         if j>1:
+#             rev = str(j)+" "+rev
+#     print(rev+res)
+'''
+         * 
+       * *
+     * * *
+   * * * *
+ * * * * *
+'''
+rows = int(input("Enter rows: "))
+for i in range(1,rows+1):
+    print(" "*(2*(rows-i)),end="")
+    print("* "*(i))
+'''
+    * * * * * 
+      * * * *
+        * * *
+          * *
+            *
+''' 
+rows = int(input("Enter rows: "))
+for i in range(rows,-1,-1):
+    print(" "*(2*(rows-i)),end="")
+    print("* "*(i))
