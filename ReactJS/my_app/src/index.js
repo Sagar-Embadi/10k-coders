017701b09b66cb83487d1b451b399539cfb335e5
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-11 13:23:45 +0530

    update

--- ReactJS/my_app/src/index.js
+++ ReactJS/my_app/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Task1 } from './Tasks/task1/task1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Task1></Task1>
   </React.StrictMode>
 );
 
